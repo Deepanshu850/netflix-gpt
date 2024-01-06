0dc6ac0cf4e0888688de6774d7e17d532b48e916
Deepanshu850 <deepuanshu123as@gmail.com>
2024-01-06 19:53:55 +0530

    setting up react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import Body from "./components/Body";
+
 export default function App() {
   return (
-    <h1 className="text-3xl text-center text-green-800 font-bold underline">
-      Namaste React</h1>
+    <Body/> 
   )
 }
